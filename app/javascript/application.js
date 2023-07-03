5bb3f99db891b755067637c72bac59244f77ee1c
Pearlicia <feliciaebikon@gmail.com>
2023-07-03 15:41:07 +0100

    remove action text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,20 +4,20 @@ import "controllers"
 // import "font-awesome";
 
 
-import "trix"
-import "@rails/actiontext"
-import "@simonbooth/actiontext";
+// import "trix"
+// import "@rails/actiontext"
+// import "@simonbooth/actiontext";
 
 // Initialize Trix or Rich Text editors
-document.addEventListener("turbolinks:load", function() {
-    var editor = document.querySelector("trix-editor");
-    if (editor) {
-      editor.addEventListener("trix-file-accept", function(event) {
-        var acceptedTypes = ["image/jpeg", "image/png", "image/gif"];
-        if (!acceptedTypes.includes(event.file.type)) {
-          event.preventDefault();
-          alert("Only support attachment of image files");
-        }
-      });
-    }
-});
+// document.addEventListener("turbolinks:load", function() {
+//     var editor = document.querySelector("trix-editor");
+//     if (editor) {
+//       editor.addEventListener("trix-file-accept", function(event) {
+//         var acceptedTypes = ["image/jpeg", "image/png", "image/gif"];
+//         if (!acceptedTypes.includes(event.file.type)) {
+//           event.preventDefault();
+//           alert("Only support attachment of image files");
+//         }
+//       });
+//     }
+// });
